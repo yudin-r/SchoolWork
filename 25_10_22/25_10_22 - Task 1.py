aa61774e773b3fbbd7e4cb08c07c7a5b65af5617
yudin <yudin.rs@yandex.ru>
2022-10-22 22:42:26 +0300

    Добавлены файлы на 25.10.22

---
+++ 25_10_22/25_10_22 - Task 1.py
@@ -0,0 +1,12 @@
+from turtle import *
+
+left(90)
+for i in range(4):
+    for j in range(4):
+        for k in range(4):
+            forward(30)
+            right(120)
+        forward(30)
+    forward(30)
+
+# ans 9
---
+++ 25_10_22/25_10_22 - Task 2.py
@@ -0,0 +1,10 @@
+import time
+from turtle import *
+
+left(90)
+for i in range(20):
+    forward(50)
+    right(30)
+    time.sleep(1)
+
+# ans 12
